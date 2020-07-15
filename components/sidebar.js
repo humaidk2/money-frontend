38ae9e2c38dd2b2ebb4c8980dec10f7b1b0db708
Humaid Khan <humaidk2@gmail.com>
2020-07-15 23:54:33 +0400

    Add link for redirecting on logout add useRouter hook for redirecting Add logout method to redirect by post request on logout

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,33 +1,53 @@
-const Sidebar = (props) => (
-  <ul className="sidebar-nav">
-    <li className="sidebar-brand">
-      <a href="#">Money.io</a>
-    </li>
-    <li>
-    <img className='profilepic' src='https://dyn0.media.forbiddenplanet.com/products/102701.jpg.square-true_maxheight-285_size-285.jpg'></img>
-    </li>
-    <li>
-     <p className='username'>Chris</p>
-    </li>
-    <li>
-      <a onClick={props.click}href="#">Transactions</a>
-    </li>
-    <li>
-      <a onClick={props.click}href="#">Owes/Debts</a>
-    </li>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <li>
-      <a href='logout'>Sign Out</a>
-    </li>
-  </ul>
-)
+import Link  from 'next/Link'
+import {useRouter} from 'next/router'
+
+const Sidebar = () => {
+  const router = useRouter()
+  const onLogout = (evt) => {
+    evt.preventDefault();
+    fetch('http://localhost:8000/logout',
+    {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'GET',
+        credentials: 'include'
+    }).then((data)=> {
+        if(data) {
+            console.log(data);
+            router.push('/signin', undefined, { shallow: true });
+        }
+    } )
+  }
+  return (
+    <ul className="sidebar-nav">
+      <li className="sidebar-brand">
+        <a href="#">Money.io</a>
+      </li>
+      <li>
+      <img className='profilepic' src='https://dyn0.media.forbiddenplanet.com/products/102701.jpg.square-true_maxheight-285_size-285.jpg'></img>
+      </li>
+      <li>
+      <p className='username'>Chris</p>
+      </li>
+      <li>
+        <Link href="/transactions"><a>Transactions</a></Link>
+      </li>
+      <li>
+        <Link href="/debts"><a>Owes/Debts</a></Link>
+      </li>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <li>
+        <button href='logout' onClick={onLogout}>Sign Out</button>
+      </li>
+    </ul>
+  );
+}
 
 export default Sidebar
