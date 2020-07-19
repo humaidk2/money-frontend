bfd0d195c0a263264f447706488a5c4350856b95
Humaid Khan <humaidk2@gmail.com>
2020-07-20 00:46:42 +0400

    Add logs for debugging props Set key for mapping listentries using spending id

--- components/spending/spendingListEntry.js
+++ components/spending/spendingListEntry.js
@@ -1,10 +1,32 @@
-const SpendingListEntry = (props) => (
-  <tr>
-    <td><img height='40' src={pickCategories(props.entry.category)}></img></td>
-    <td>{props.entry.title}</td>
-    <td>{props.entry.amount}</td>
-    <td>{moment(props.entry.created_at).fromNow()}</td>
-  </tr>
-)
+const SpendingListEntry = (props) => {
+  const {id, category, title, amount, created_at} = props.entry
+  const pickCategories = (category) => {
+    if (category === 'Restaurant') {
+      return 'https://www.iconsdb.com/icons/preview/black/restaurant-xxl.png';
+    } else if (category === 'Shopping') {
+      return 'https://d30y9cdsu7xlg0.cloudfront.net/png/17663-200.png';
+    } else if (category === 'Transportation') {
+      return 'http://image.flaticon.com/icons/png/128/67/67994.png';
+    } else if (category === 'Groceries') {
+      return 'http://image.flaticon.com/icons/png/512/2/2772.png';
+    } else if (category === 'Utilities') {
+      return 'http://www.iconsdb.com/icons/preview/black/house-xxl.png';
+    } else if (category === 'Nightlife') {
+      return 'http://www.clker.com/cliparts/I/n/S/M/8/z/cocktail-md.png';
+    } else if (category === 'Other') {
+      return 'https://d30y9cdsu7xlg0.cloudfront.net/png/203819-200.png';
+    }
+  }
+  console.log(props)
+  console.log("id " + id)
+  return (
+    <tr>
+      <td><img height='40' src={pickCategories(category)}></img></td>
+      <td>{title}</td>
+      <td>{"$ " + amount}</td>
+      <td>{moment(created_at).fromNow()}</td>
+    </tr>
+  );
+}
 
 export default SpendingListEntry
