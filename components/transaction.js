24471351fee6a831f61cd5854679b9e44ec494c9
Humaid Khan <humaidk2@gmail.com>
2020-07-19 19:47:34 +0400

    Remove unused methods Rename component to Transactions for semantics Remove testing header for cleanup Use boolean logic to display transactions if they are available Update styles to change background to white for transaction component body

--- components/transaction.js
+++ components/transaction.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 
 import useSWR from 'swr'
 const fetcher = (url) => fetch(url,{method: 'GET',credentials: 'include' }).then(res => res.json());
-export default function App(props) {
+export default function Transactions(props) {
     const router = useRouter()
     const url = "http://localhost:8000/transactions"
     const { data, error } = useSWR(url, fetcher);
@@ -13,49 +13,25 @@ export default function App(props) {
   if (!data) return <div>Loading...</div>;
   if(data) { console.log(data)}
   if(data && !data.Message) { router.push('/signin', undefined, { shallow: true }) }
-
-//   constructor(props) {
-//     super(props);
-//     this.submit = this.submit.bind(this);
-//     this.state = {
-//       signedIn: false,
-//       data: this.props.data,
-//       transactions: [],
-//       debts: []
-//     };
-//   }
-  
-
-//   loadTrans(data) {
-//     this.setState({
-//       transactions: data.transaction
-//     });
-
-//     this.setState({
-//       curr: (<SpendingList list={this.state.transactions} submit={this.submit} />)
-//     });
-//   }
-
-
-//   componentDidMount() {
-//     this.props.getReq({url: 'localhost:8000/transactions'}, this.loadTrans.bind(this));
-//   }
-
-
-//   submit(e) {
-//     e.preventDefault();
-
-//   }
 return (
     <div id="wrapper" className="app">
-        <h1>{JSON.stringify(data)}</h1>
         <div id='sidebar-wrapper'>
             <Sidebar />
         </div>
         <div id='page-content-wrapper'>
             <h1 className='appTitle'><strong>Money</strong>.io</h1>
-            <SpendingList list={props.data} />
+            {data && data.Message && <SpendingList list={data.transactions} />}
         </div>
+        <style global jsx>
+        {`
+        body {
+            background: white;
+        }
+        #wrapper {
+            background: white;
+        }
+        `}
+        </style>
     </div>
 );
 
