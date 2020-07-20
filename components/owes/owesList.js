06a7e943ffee9f6a118b9d7aa4985782bf1d773c
Humaid Khan <humaidk2@gmail.com>
2020-07-20 19:25:11 +0400

    Prettify code for cleanliness, Remove Loans form

--- components/owes/owesList.js
+++ components/owes/owesList.js
@@ -1,29 +1,30 @@
-import OwesListEntry from './owesListEntry'
-import DuesForm from './duesForm'
-import LoansForm from './loansForm'
+import OwesListEntry from "./owesListEntry";
+import DuesForm from "./duesForm";
+import LoansForm from "./loansForm";
 const OwesList = (props) => {
-  return(
-  <div className="owes">
-    <h2>Dues</h2>
-    <table className="table">
-      <thead id="owes-head">
-        <tr>
-          <th>Who</th>
-          <th>Title</th>
-          <th>Amount</th>
-          <th>Date</th>
-          <th>Option</th>
-        </tr>
-      </thead>
-      <tbody>
-      {props.list.map(owes => <OwesListEntry key={owes.id} entry={owes} />)}
-      </tbody>
-    </table>
-    <DuesForm />
-    <br />
-    <LoansForm />
-  </div>
+  return (
+    <div className="owes">
+      <h2>Dues</h2>
+      <table className="table">
+        <thead id="owes-head">
+          <tr>
+            <th>Who</th>
+            <th>Title</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Option</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.list.map((owes) => (
+            <OwesListEntry key={owes.id} entry={owes} />
+          ))}
+        </tbody>
+      </table>
+      <DuesForm />
+    </div>
   );
-  }
+};
 
-export default OwesList
+export default OwesList;
