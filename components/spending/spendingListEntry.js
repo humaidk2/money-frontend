dc223d252d36de7bef1d672070813b196cd0deb7
Humaid Khan <humaidk2@gmail.com>
2020-07-29 17:04:40 +0400

    Add delete button styles Separate iconpicker method for clarity Call deleteClick from props that is passed down using component composition, giving access to the state

--- components/spending/spendingListEntry.js
+++ components/spending/spendingListEntry.js
@@ -1,32 +1,6 @@
 const SpendingListEntry = (props) => {
   const { id, category, title, amount, created_at } = props.entry;
-  const pickCategories = (category) => {
-    if (category === "Restaurant") {
-      return "https://www.iconsdb.com/icons/preview/black/restaurant-xxl.png";
-    } else if (category === "Shopping") {
-      return "https://d30y9cdsu7xlg0.cloudfront.net/png/17663-200.png";
-    } else if (category === "Transportation") {
-      return "http://image.flaticon.com/icons/png/128/67/67994.png";
-    } else if (category === "Groceries") {
-      return "http://image.flaticon.com/icons/png/512/2/2772.png";
-    } else if (category === "Utilities") {
-      return "http://www.iconsdb.com/icons/preview/black/house-xxl.png";
-    } else if (category === "Nightlife") {
-      return "http://www.clker.com/cliparts/I/n/S/M/8/z/cocktail-md.png";
-    } else if (category === "Other") {
-      return "https://d30y9cdsu7xlg0.cloudfront.net/png/203819-200.png";
-    }
-  };
-  const deleteTransaction = () => {
-    fetch("http://localhost:8000/deleteTransactions", {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      body: JSON.stringify({
-        id: props.entry.id,
-      }),
-      credentials: "include",
-    });
-  };
+  const { onDeleteClick } = this.props;
   return (
     <tr>
       <td>
@@ -38,14 +12,38 @@ const SpendingListEntry = (props) => {
       <td>
         <button
           type="button"
-          onClick={deleteTransaction}
+          onClick={onDeleteClick}
           className="btn btn-danger"
         >
           Delete
         </button>
       </td>
+      <style jsx>{`
+        .btn-danger {
+          background-color: #c82333;
+          border-color: #bd2130;
+        }
+      `}</style>
     </tr>
   );
+};
+
+const pickCategories = (category) => {
+  if (category === "Restaurant") {
+    return "https://www.iconsdb.com/icons/preview/black/restaurant-xxl.png";
+  } else if (category === "Shopping") {
+    return "https://d30y9cdsu7xlg0.cloudfront.net/png/17663-200.png";
+  } else if (category === "Transportation") {
+    return "http://image.flaticon.com/icons/png/128/67/67994.png";
+  } else if (category === "Groceries") {
+    return "http://image.flaticon.com/icons/png/512/2/2772.png";
+  } else if (category === "Utilities") {
+    return "http://www.iconsdb.com/icons/preview/black/house-xxl.png";
+  } else if (category === "Nightlife") {
+    return "http://www.clker.com/cliparts/I/n/S/M/8/z/cocktail-md.png";
+  } else if (category === "Other") {
+    return "https://d30y9cdsu7xlg0.cloudfront.net/png/203819-200.png";
+  }
 };
 
 export default SpendingListEntry;
