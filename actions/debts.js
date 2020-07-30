f4fa8090c0fcd537b9abe542a8b01471bd434072
Humaid Khan <humaidk2@gmail.com>
2020-07-30 16:28:03 +0400

    Dispatch failure if status is not 200 using reject

--- actions/debts.js
+++ actions/debts.js
@@ -1,54 +1,63 @@
 import * as api from "../api/debts";
 
 export const fetchDebts = () => (dispatch) => {
-  api.fetchDebts().then(
-    (response) => {
-      dispatch({
-        type: "FETCH_DEBTS_SUCCESS",
-        response,
-      });
-    },
-    (error) => {
-      dispatch({
-        type: "FETCH_DEBTS_FAILURE",
-        message: error.message || "Something went wrong.",
-      });
-    }
-  );
+  api
+    .fetchDebts()
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        dispatch({
+          type: "FETCH_DEBTS_SUCCESS",
+          response,
+        });
+      },
+      (error) => {
+        dispatch({
+          type: "FETCH_DEBTS_FAILURE",
+          message: error.message || "Something went wrong.",
+        });
+      }
+    );
 };
 
 export const addDebt = (type, person, amount, description) => (dispatch) => {
-  api.addDebt(type, person, amount, description).then(
-    (response) => {
-      console.log(response);
-      dispatch({
-        type: "ADD_DEBT_SUCCESS",
-        response,
-      });
-    },
-    (error) => {
-      dispatch({
-        type: "ADD_DEBT_FAILURE",
-        message: error.message || "Something went wrong.",
-      });
-    }
-  );
+  api
+    .addDebt(type, person, amount, description)
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        console.log(response);
+        dispatch({
+          type: "ADD_DEBT_SUCCESS",
+          response,
+        });
+      },
+      (error) => {
+        dispatch({
+          type: "ADD_DEBT_FAILURE",
+          message: error.message || "Something went wrong.",
+        });
+      }
+    );
 };
 
 export const deleteDebt = (id) => (dispatch) => {
-  api.deleteDebt(id).then(
-    (response) => {
-      console.log(response);
-      dispatch({
-        type: "DELETE_DEBT_SUCCESS",
-        response,
-      });
-    },
-    (error) => {
-      dispatch({
-        type: "DELETE_DEBT_FAILURE",
-        message: error.message || "Something went wrong.",
-      });
-    }
-  );
+  api
+    .deleteDebt(id)
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        console.log(response);
+        dispatch({
+          type: "DELETE_DEBT_SUCCESS",
+          response,
+        });
+      },
+      (error) => {
+        dispatch({
+          type: "DELETE_DEBT_FAILURE",
+          message: error.message || "Something went wrong.",
+        });
+      }
+    );
 };
--- actions/transactions.js
+++ actions/transactions.js
@@ -5,57 +5,79 @@ export const fetchTransactions = () => (dispatch) => {
   //     type: "FETCH_TRANSACTIONS_REQUEST",
   //   });
   console.log("fetching");
-  api.fetchTransactions().then(
-    (response) => {
-      dispatch({
-        type: "FETCH_TRANSACTIONS_SUCCESS",
-        response,
-      });
-    },
-    (error) => {
-      console.log(error.status);
-      console.log(error.status == 401);
-      console.log(error);
-      dispatch({
-        type: "FETCH_TRANSACTIONS_FAILURE",
-        message: error.message || "Something went wrong.",
-      });
-    }
-  );
+  api
+    .fetchTransactions()
+    .then((response) => {
+      console.log(response.status);
+      if (response.status !== 200) {
+        return Promise.reject(response);
+      } else return response.json();
+    })
+    .then(
+      (response) => {
+        dispatch({
+          type: "FETCH_TRANSACTIONS_SUCCESS",
+          response,
+        });
+      },
+      (error) => {
+        if (error.status === 401) {
+          dispatch({
+            type: "LOGIN_FAILURE",
+          });
+        } else {
+          dispatch({
+            type: "FETCH_TRANSACTIONS_FAILURE",
+            message: error.message || "Something went wrong.",
+          });
+        }
+      }
+    );
 };
 
 export const addTransaction = (category, title, amount) => (dispatch) => {
-  api.addTransaction(category, title, amount).then(
-    (response) => {
-      console.log(response);
-      dispatch({
-        type: "ADD_TRANSACTION_SUCCESS",
-        response,
-      });
-    },
-    (error) => {
-      dispatch({
-        type: "ADD_TRANSACTION_FAILURE",
-        message: error.message || "Something went wrong.",
-      });
-    }
-  );
+  api
+    .addTransaction(category, title, amount)
+    .then((response) => {
+      console.log(response.status);
+      if (response.status === 401) {
+        throw new Error("Oh no!");
+      } else return response.json();
+    })
+    .then(
+      (response) => {
+        dispatch({
+          type: "ADD_TRANSACTION_SUCCESS",
+          response,
+        });
+      },
+      (error) => {
+        console.log(error);
+        dispatch({
+          type: "ADD_TRANSACTION_FAILURE",
+          message: error.message || "Something went wrong.",
+        });
+      }
+    );
 };
 
 export const deleteTransaction = (id) => (dispatch) => {
-  api.deleteTransaction(id).then(
-    (response) => {
-      console.log(response);
-      dispatch({
-        type: "DELETE_TRANSACTION_SUCCESS",
-        response,
-      });
-    },
-    (error) => {
-      dispatch({
-        type: "DELETE_TRANSACTION_FAILURE",
-        message: error.message || "Something went wrong.",
-      });
-    }
-  );
+  api
+    .deleteTransaction(id)
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        console.log(response);
+        dispatch({
+          type: "DELETE_TRANSACTION_SUCCESS",
+          response,
+        });
+      },
+      (error) => {
+        dispatch({
+          type: "DELETE_TRANSACTION_FAILURE",
+          message: error.message || "Something went wrong.",
+        });
+      }
+    );
 };
