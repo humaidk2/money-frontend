3c7b40faff7509203c7716a9af2dfcc0becad059
Humaid Khan <humaidk2@gmail.com>
2020-08-08 17:40:51 +0400

    Add react-google-login button to get access token on successful google login Show error message on failed login

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,14 +2,17 @@ import Link from "next/link";
 import * as actions from "../actions/auth";
 import { connect } from "react-redux";
 import { AvatarGenerator } from "random-avatar-generator";
-const Sidebar = ({ username, logout }) => {
+import { GoogleLogout } from "react-google-login";
+const Sidebar = ({ username, logout, isGoogleLoggedIn }) => {
   const generator = new AvatarGenerator();
   const onLogout = (evt) => {
     evt.preventDefault();
+    console.log("logout success");
     logout();
   };
+  const onGoogleLogout = (evt) => {};
   return (
-    <ul className="sidebar-nav">
+    <ul className="sidebar-nav" data-toggle="collapse">
       <li className="sidebar-brand">
         <a href="#">Money.io</a>
       </li>
@@ -42,15 +45,24 @@ const Sidebar = ({ username, logout }) => {
       <br />
       <br />
       <li>
-        <button href="logout" onClick={onLogout}>
-          Sign Out
-        </button>
+        {isGoogleLoggedIn ? (
+          <GoogleLogout
+            clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={logout}
+          ></GoogleLogout>
+        ) : (
+          <button href="logout" onClick={onLogout}>
+            Sign Out
+          </button>
+        )}
       </li>
     </ul>
   );
 };
 const mapStateToProps = (state) => ({
   username: state.isLoggedIn,
+  isGoogleLoggedIn: state.isGoogleLoggedIn,
 });
 
 export default connect(mapStateToProps, actions)(Sidebar);
